b249f57326c214729eb61b4c186863bef4b53807
markgreenburg <marek.greenburg@gmail.com>
2017-02-02 23:49:24 -0600

    added typing notification

--- socketExample/app.js
+++ socketExample/app.js
@@ -9,24 +9,39 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+let users = [];
+
 io.on("connection", (socket) => {
     // When username received, broadcast notice to all other connections
     socket.on("entered chatroom", (nickname) => {
-        socket.broadcast.emit("entered chatroom", nickname + " has joined");
         socket.username = nickname;
+        socket.broadcast.emit("entered chatroom", socket.username);
     });
     
     // When new chat message received, broadcast it to all other connections
     socket.on("chat message", (msg) => {
-        socket.broadcast.emit("chat message", socket.username + ": " + msg)
+        socket.broadcast.emit("chat message", msg)
+    });
+
+    // When user done typing, broadcast info to all other connections
+    socket.on("done typing", (user) => {
+        socket.broadcast.emit("typing finished", socket.username);
+    });
+
+    // When user is typing, broadcast that fact to all other connections
+    socket.on("started typing", () => {
+        socket.broadcast.emit("user typing", socket.username);
     });
 
     // When user disconnects, broadcast notice to all other connections
     socket.on("disconnect", () => {
-        socket.broadcast.emit("disconnect notice", socket.username + 
-            " has left");
+        socket.broadcast.emit("disconnect notice", socket.username);
+        users.filter(removeDisconnected);
     });
 });
+
+// Remove disconnected users
+const removeDisconnected = (user) => (user !== socket.username);
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
--- socketExample/public/index.js
+++ socketExample/public/index.js
@@ -1,12 +1,15 @@
 $(() => {
-    var socket = io();
+    let socket = io();
+    let typingTimeout = 0;
+    let users = [];
 
     /** 
     * Emiters
     */
     // When user's nickname is submitted
     $("form#nickname-form").submit(() => {
-        socket.emit("entered chatroom", $("input#nickname-input").val());
+        socket.username = $("input#nickname-input").val();
+        socket.emit("entered chatroom", socket.username);
         $("div#nickname-div").hide();
         $("div#chat-div").show();
         return false;
@@ -14,6 +17,7 @@ $(() => {
     
     // When user's chat message is submitted
     $("form#chat-form").submit(() => {
+        socket.emit("done typing", socket.username);
         $("#messages").append($("<li class='right'>").text($("#m").val()));
         socket.emit("chat message", $("#m").val());
         $("#m").val("");
@@ -22,31 +26,52 @@ $(() => {
 
     // When user starts typing
     $("form#chat-form").keydown(() => {
-        socket.emit("typing");
+        socket.emit("started typing");
+        return true;
+    });
+
+    // When user stops typing
+    $("form#chat-form").keyup(() => {
+        socket.emit("finished typing", false);
         return true;
     });
 
     /**
     * Listeners
     */
-    // New chat message received
-    socket.on("chat message", (msg) => {
-        $("#messages").append($("<li>").text(msg));
-    });
-
     // New connection to chat room
-    socket.on("entered chatroom", (msg) => {
-        $("#flashes").append($("<li>").text(msg));
+    socket.on("entered chatroom", (user) => {
+        $("#flashes").append($("<li>").text(user + " has joined!"));
         removeFirstFlash();       
     });
 
+    // New chat message received
+    socket.on("chat message", (msg) => {
+        $("#messages").append($("<li>").text(socket.username + ": " + msg));
+    });
+
+    // User typing notification
+    socket.on("user typing", (user) => {
+        $("div#is-typing").show().text(user +  " is typing...");
+        clearTimeout(typingTimeout);
+        typingTimeout = (setTimeout(() => {
+            $("div#is-typing").hide();
+        }, 2000));
+    });
+
+    // User done typing notification (when form submitted)
+    socket.on("typing finished", (user) => {
+        $("div#is-typing").hide();
+    });
+
     // User disconnects from chat room
-    socket.on("disconnect notice", (msg) => {
-        $("#flashes").append($("<li>").text(msg));
+    socket.on("disconnect notice", (user) => {
+        $("#flashes").append($("<li>").text(user + " has left"));
         removeFirstFlash();
-    })
+    });
 });
 
+/* Timeout Helpers */
 // Removes first flash from list in list after 5 seconds
 const removeFirstFlash = () => {
     setTimeout(() => {
